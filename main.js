2ac327c775c5e6da8be4f1b2d2c872aab0d493c0
Travis CI User <travis@example.org>
2023-08-23 07:37:13 +0000

    publish: Merge pull request #43 from konnectors/fix/SortDetailed
    generated from commit 188963a5da6908b04ddf25a11c4f9fca20c88b03

--- main.js
+++ main.js
@@ -5634,10 +5634,17 @@ class RedContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_M
       await this.saveCredentials(this.store.userCredentials)
     }
     await this.waitForElementInWorker(`a[href="${INFO_CONSO_URL}"]`)
-    await this.clickAndWait(
-      `a[href="${INFO_CONSO_URL}"]`,
-      `a[href="${BILLS_URL_PATH}"]`
-    )
+    await this.runInWorker('click', `a[href="${INFO_CONSO_URL}"]`)
+    await Promise.race([
+      this.waitForElementInWorker(`a[href="${BILLS_URL_PATH}"]`),
+      this.waitForElementInWorker('#password')
+    ])
+    // Sometimes when reaching the bills page, website ask for a re-authentication.
+    // As we cannot do an autoLogin or autoFill, we just show the page to the user so he can make the login confirmation
+    const askRelogin = await this.isElementInWorker('#password')
+    if (askRelogin) {
+      await this.waitForUserAuthentication()
+    }
     await this.clickAndWait(
       `a[href="${BILLS_URL_PATH}"]`,
       'button[onclick="plusFacture(); return false;"]'
@@ -5647,12 +5654,22 @@ class RedContentScript extends cozy_clisk_dist_contentscript__WEBPACK_IMPORTED_M
     this.log('debug', 'Saving files')
     await this.saveIdentity(this.store.userIdentity)
     for (const bill of this.store.allBills) {
-      await this.saveBills([bill], {
-        context,
-        fileIdAttributes: ['filename'],
-        contentType: 'application/pdf',
-        qualificationLabel: 'phone_invoice'
-      })
+      if (bill.filename.includes('détail')) {
+        await this.saveBills([bill], {
+          context,
+          fileIdAttributes: ['filename'],
+          contentType: 'application/pdf',
+          qualificationLabel: 'phone_invoice',
+          subPath: 'Detailed Invoices'
+        })
+      } else {
+        await this.saveBills([bill], {
+          context,
+          fileIdAttributes: ['filename'],
+          contentType: 'application/pdf',
+          qualificationLabel: 'phone_invoice'
+        })
+      }
     }
   }
 
